8196224df3b5b19b8526d444f2fdf34218eb9ea6
osukhorukova <o.skhrkv@gmail.com>
2024-03-10 21:41:14 +0300

    Selenium Locators Homework

--- src/test/java/LocatorsTest.java
+++ src/test/java/LocatorsTest.java
@@ -2,25 +2,25 @@ import configuration.ReadProperties;
 import core.BrowserService;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.*;
 
 
 public class LocatorsTest {
     private WebDriver driver;
 
-    @BeforeMethod
+    @BeforeTest
     public void setup() {
         driver = new BrowserService().getDriver();
     }
 
-    @AfterMethod
+    @AfterTest
     public void tearDown() {
         driver.quit();
     }
 
-    @Test (testName = "Signing in as a standard user")
+    @Test(testName = "Signing in as a standard user")
     public void locatorsBasicTest_1() throws InterruptedException {
         driver.get(ReadProperties.getUrl());
 
@@ -35,18 +35,41 @@ public class LocatorsTest {
         Thread.sleep(1000);
     }
 
-        @Test (testName = "Adding items to cart")
-        public void locatorsBasicTest_2() throws InterruptedException {
-            driver.get(ReadProperties.getUrl());
+    @Test(testName = "Adding items to cart using basic locators", dependsOnMethods = "locatorsBasicTest_1")
+    public void locatorsBasicTest_2() throws InterruptedException {
 
-            // Find webElement by tagName - sorting the list by price (low to high)
-            driver.findElement(By.tagName("select")).click();
+        // Find webElement by tagName - sorting the list by price (low to high)
+        WebElement sorting = driver.findElement(By.tagName("select"));
+        sorting.click();
+        Thread.sleep(1000);
+        Select selectSort = new Select(sorting);
+        selectSort.selectByValue("lohi");
+        Thread.sleep(1000);
 
-            // Find webElement by linkText
-            driver.findElement(By.linkText("Forgot your password?")).click();
+        // Find webElement by linkText - clicking on the item "Sauce Labs Onesie"
+        driver.findElement(By.linkText("Sauce Labs Onesie")).click();
+        Thread.sleep(1000);
 
-            // Find webElement by linkText
-            driver.findElement(By.partialLinkText("your password?")).click();
-        }
+        // Find webElement by id - adding the item "Sauce Labs Onesie" to cart
+        driver.findElement(By.id("add-to-cart-sauce-labs-onesie")).click();
+        Thread.sleep(1000);
+
+        // Find webElement by id - going back to Products page
+        driver.findElement(By.id("back-to-products")).click();
+        Thread.sleep(1000);
+
+        // Find webElement by partialLinkText - clicking on the item "Sauce Labs Backpack"
+        driver.findElement(By.partialLinkText("Labs Backpack")).click();
+        Thread.sleep(1000);
+
+        // Find webElement by id - going back to Products page
+        driver.findElement(By.id("back-to-products")).click();
+        Thread.sleep(1000);
+    }
+
+//    @Test(testName = "Adding items to cart using CSS locators", dependsOnMethods = "locatorsBasicTest_1")
+//    public void locatorsCssTest() throws InterruptedException {
+//
+//    }
 }
 
