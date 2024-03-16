cbcbc70d7cdab60c2514e42031a35e8310ffeae8
osukhorukova <o.skhrkv@gmail.com>
2024-03-16 22:33:06 +0300

    Selenium Locators Homework

--- src/test/java/CssLocatorsTest.java
+++ src/test/java/CssLocatorsTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -26,38 +27,46 @@ public class CssLocatorsTest {
     }
 
     @AfterTest
-    public void tearDown() {driver.quit();
+    public void tearDown() {
+        driver.quit();
     }
 
     @Test(testName = "Adding items to cart using CSS locators")
-    public void locatorsCssTest_2() throws InterruptedException {
+    public void locatorsCssTest_1() throws InterruptedException {
 
-        // Find webElement by tagName - sorting the list by price (low to high)
-        WebElement sorting = driver.findElement(By.tagName("select"));
+        //by css tagName - sorting the list by name (Z to A)
+        WebElement sorting = driver.findElement(By.cssSelector("select"));
         sorting.click();
         Thread.sleep(1000);
         Select selectSort = new Select(sorting);
-        selectSort.selectByValue("lohi");
+        selectSort.selectByValue("za");
         Thread.sleep(1000);
-
-        // Find webElement by linkText - clicking on the item "Sauce Labs Onesie"
-        driver.findElement(By.linkText("Sauce Labs Onesie")).click();
+        //by .class - opening the hamburger menu
+        driver.findElement(By.cssSelector(".bm-burger-button")).click();
         Thread.sleep(1000);
-
-        // Find webElement by id - adding the item "Sauce Labs Onesie" to cart
-        driver.findElement(By.id("add-to-cart-sauce-labs-onesie")).click();
+        //by tagName.class - closing the hamburger menu
+        driver.findElement(By.cssSelector("div.bm-cross-button")).click();
         Thread.sleep(1000);
-
-        // Find webElement by id - going back to Products page
-        driver.findElement(By.id("back-to-products")).click();
+        //by [attribute=value] - adding "Sauce Labs Backpack" to cart
+        driver.findElement(By.cssSelector("[data-test='add-to-cart-sauce-labs-backpack']")).click();
         Thread.sleep(1000);
-
-        // Find webElement by partialLinkText - clicking on the item "Sauce Labs Backpack"
-        driver.findElement(By.partialLinkText("Labs Backpack")).click();
+        //by .class1 .class2 - opening cart
+        driver.findElement(By.cssSelector(".primary_header .shopping_cart_container")).click();
         Thread.sleep(1000);
-
-        // Find webElement by id - going back to Products page
-        driver.findElement(By.id("back-to-products")).click();
+        //by [attribute$=value] - checking the items counter on the cart navbar icon
+        Assert.assertEquals(driver.findElement(By.cssSelector("[class$='badge']")).getText(), "1");
+        Thread.sleep(1000);
+        //by [attribute|=value] - going back to shopping
+        driver.findElement(By.cssSelector("[name|='continue']")).click();
+        Thread.sleep(1000);
+        //by [attribute^=value] - checking the presence of twitter link
+        Assert.assertTrue(driver.findElement(By.cssSelector("[href^='https://twitter']")).isDisplayed());
+        Thread.sleep(1000);
+        //by [] - checking the presence of facebook link
+        Assert.assertTrue(driver.findElement(By.cssSelector("[class~='social_facebook']")).isDisplayed());
+        Thread.sleep(1000);
+        //by [attribute*=value] - checking the presence of linkedin link
+        Assert.assertTrue(driver.findElement(By.cssSelector("[href*='linkedin']")).isDisplayed());
         Thread.sleep(1000);
     }
 }
