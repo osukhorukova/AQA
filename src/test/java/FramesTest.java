f3d8ca295c1548cff5702ca879a7e0deeb6c8305
osukhorukova <o.skhrkv@gmail.com>
2024-03-24 16:21:44 +0300

    Selenium Locators Homework

--- src/test/java/FramesTest.java
+++ src/test/java/FramesTest.java
@@ -10,7 +10,7 @@ public class FramesTest extends BaseTest {
     public void frameTest()  {
         driver.get("https://the-internet.herokuapp.com/frames");
         driver.findElement(By.xpath("//*[@id='content']/div/ul/li[2]/a")).click();
-        driver.switchTo().frame(0);
+        driver.switchTo().frame("mce_0_ifr");
         WebElement placeholder = driver.findElement(By.xpath("//*[@id='tinymce']/p"));
         Assert.assertEquals(placeholder.getText(),"Your content goes here.");
         driver.switchTo().parentFrame();
---
+++ src/test/java/WindowsTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class WindowsTest extends BaseTest {
+    @Test
+    public void frameTest() {
+        driver.get("https://the-internet.herokuapp.com/windows");
+
+        String originalWindowHandle = driver.getWindowHandle();
+
+        driver.findElement(By.linkText("Click Here")).click();
+        driver.findElement(By.linkText("Click Here")).click();
+
+        Set<String> windowHandleSet = driver.getWindowHandles();
+        List<String> windowHandleList = new ArrayList<>(windowHandleSet);
+
+        driver.switchTo().window(windowHandleList.get(1));
+        driver.switchTo().window(windowHandleList.get(2));
+
+        Assert.assertEquals(waitsService.waitForVisibilityLocatedBy(By.tagName("h3")).getText(), "New Window");
+        driver.close();
+        driver.switchTo().window(originalWindowHandle);
+        Assert.assertEquals(driver.getWindowHandles().size(),2);
+    }
+}
